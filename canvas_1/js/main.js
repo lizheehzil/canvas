dad842c44b46fd963af29fa95898f78b348540d6
lizhe <442519654@qq.com>
2016-09-07 23:29:35 +0800

    测试了圆形的填充

--- canvas_1/js/main.js
+++ canvas_1/js/main.js
@@ -8,9 +8,11 @@ function canvas() {
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
     ctx.beginPath();
-    ctx.arc(100,100,50,0,360*Math.PI,true);
-    ctx.clip();
+    ctx.fillStyle="orange";
+    ctx.arc(200,100,50,0,360*Math.PI,true);
+    ctx.fill();
+    ctx.stroke();
     ctx.fillStyle="red";
-    ctx.fillRect(100,100,25,25);
+    ctx.fillRect(50,50,100,100);
 
 }
